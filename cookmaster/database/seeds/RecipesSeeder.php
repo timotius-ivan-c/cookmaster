df81cbd1d83f10025e57e2f6c53a6a0b844487ab
jessrellia <jess.aurellia@gmail.com>
2021-01-01 00:40:52 +0700

    Updated roles & recipes seeder

--- cookmaster/database/seeds/RecipesSeeder.php
+++ cookmaster/database/seeds/RecipesSeeder.php
@@ -14,11 +14,11 @@ class RecipesSeeder extends Seeder
     public function run()
     {
         DB::table('recipes')->insert([
-            ['author_id' => 10, 'name' => 'Har Gow', 'image' => 'Har_Gow.jpg', 'review_count' => 31, 'average_rating' => 5, 'publish_date' => Carbon::create(2020, 9, 29,  12, 31, 11, 'Asia\Jakarta'), 'recipe_type' => 2, 'recipe_category_id' => 1],
-            ['author_id' => 10, 'name' => 'Chinese Deviled Eggs', 'image' => 'Deviled_Eggs.jpg', 'review_count' => 31, 'average_rating' => 5, 'publish_date' => Carbon::create(2019, 7, 17,  8, 11, 21, 'Asia\Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 1],
-            ['author_id' => 10, 'name' => 'Peanut Butter Cookies', 'image' => 'PB_Cookies.jpg', 'review_count' => 165, 'average_rating' => 5, 'publish_date' => Carbon::create(2020, 12, 30,  18, 27, 03, 'Asia\Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 3],
-            ['author_id' => 10, 'name' => 'Parmesan Crusted Chicken', 'image' => 'Parmesan_Chicken.jpg', 'review_count' => 18, 'average_rating' => 5, 'publish_date' => Carbon::create(2020, 10, 16,  8, 11, 12, 'Asia\Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 3],
-            ['author_id' => 10, 'name' => 'Oreo Truffles', 'image' => 'Oreo_Truffles.jpg', 'review_count' => 15, 'average_rating' => '5', 'publish_date' => Carbon::create(2019, 8, 15,  19, 32, 11, 'Asia\Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 3],
+            ['author_id' => 10, 'name' => 'Har Gow', 'image' => 'Har_Gow.jpg', 'review_count' => 31, 'average_rating' => 4.89, 'publish_date' => Carbon::create(2020, 9, 29,  12, 31, 11, 'Asia/Jakarta'), 'recipe_type' => 2, 'recipe_category_id' => 1],
+            ['author_id' => 10, 'name' => 'Chinese Deviled Eggs', 'image' => 'Deviled_Eggs.jpg', 'review_count' => 31, 'average_rating' => 4.83, 'publish_date' => Carbon::create(2019, 7, 17,  8, 11, 21, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 1],
+            ['author_id' => 10, 'name' => 'Peanut Butter Cookies', 'image' => 'PB_Cookies.jpg', 'review_count' => 165, 'average_rating' => 4.85, 'publish_date' => Carbon::create(2020, 12, 30,  18, 27, 03, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 3],
+            ['author_id' => 10, 'name' => 'Parmesan Crusted Chicken', 'image' => 'Parmesan_Chicken.jpg', 'review_count' => 18, 'average_rating' => 5.00, 'publish_date' => Carbon::create(2020, 10, 16,  8, 11, 12, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 3],
+            ['author_id' => 10, 'name' => 'Oreo Truffles', 'image' => 'Oreo_Truffles.jpg', 'review_count' => 15, 'average_rating' => 4.88, 'publish_date' => Carbon::create(2019, 8, 15,  19, 32, 11, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 3],
         ]);
     }
 }
--- cookmaster/database/seeds/RolesSeeder.php
+++ cookmaster/database/seeds/RolesSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RolesSeeder extends Seeder
 {
@@ -11,6 +12,10 @@ class RolesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('roles')->insert([
+            ['name' => 'Member'],
+            ['name' => 'Contributor'],
+            ['name' => 'Chef'],
+        ]);
     }
 }
