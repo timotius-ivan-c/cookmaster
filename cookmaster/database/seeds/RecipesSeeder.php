bf9ff425eb1261955e8b36d959c9c6f3cb4ebfe9
jessrellia <jess.aurellia@gmail.com>
2021-01-01 00:08:03 +0700

    new instances of user & recipes seeder

--- cookmaster/database/seeds/RecipesSeeder.php
+++ cookmaster/database/seeds/RecipesSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RecipesSeeder extends Seeder
 {
@@ -11,6 +13,12 @@ class RecipesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('recipes')->insert([
+            ['author_id' => 10, 'name' => 'Har Gow', 'image' => 'Har_Gow.jpg', 'review_count' => 31, 'average_rating' => 5, 'publish_date' => Carbon::create(2020, 9, 29,  12, 31, 11, 'Asia\Jakarta'), 'recipe_type' => 2, 'recipe_category_id' => 1],
+            ['author_id' => 10, 'name' => 'Chinese Deviled Eggs', 'image' => 'Deviled_Eggs.jpg', 'review_count' => 31, 'average_rating' => 5, 'publish_date' => Carbon::create(2019, 7, 17,  8, 11, 21, 'Asia\Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 1],
+            ['author_id' => 10, 'name' => 'Peanut Butter Cookies', 'image' => 'PB_Cookies.jpg', 'review_count' => 165, 'average_rating' => 5, 'publish_date' => Carbon::create(2020, 12, 30,  18, 27, 03, 'Asia\Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 3],
+            ['author_id' => 10, 'name' => 'Parmesan Crusted Chicken', 'image' => 'Parmesan_Chicken.jpg', 'review_count' => 18, 'average_rating' => 5, 'publish_date' => Carbon::create(2020, 10, 16,  8, 11, 12, 'Asia\Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 3],
+            ['author_id' => 10, 'name' => 'Oreo Truffles', 'image' => 'Oreo_Truffles.jpg', 'review_count' => 15, 'average_rating' => '5', 'publish_date' => Carbon::create(2019, 8, 15,  19, 32, 11, 'Asia\Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 3],
+        ]);
     }
 }
--- cookmaster/database/seeds/UsersSeeder.php
+++ cookmaster/database/seeds/UsersSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersSeeder extends Seeder
 {
@@ -11,6 +13,12 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('user')->insert([
+           ['id' => 6, 'name' => 'Choi Soobin', 'email' => 'soobin@gmail.com', 'password' => Hash::make('csb0512'), 'balance' => 150000, 'role_id' => 1, 'tier_id' => 4, 'fame' => 0, 'free_recipes_count' => 0, 'paid_recipes_count' => 0],
+           ['id' => 7, 'name' => 'Kim Seokjin', 'email' => 'ksjin@gmail.com', 'password' => Hash::make('wwh921204'), 'balance' => 85000, 'role_id' => 1, 'tier_id' => 3, 'fame' => 0, 'free_recipes_count' => 0, 'paid_recipes_count' => 0],
+           ['id' => 8, 'name' => 'Kim Yong-sun', 'email' => 'solarsido@gmail.com', 'password' => Hash::make('mmm0611'), 'balance' => 33500, 'role_id' => 1, 'tier_id' => 2, 'fame' => 0, 'free_recipes_count' => 0, 'paid_recipes_count' => 0],
+           ['id' => 9, 'name' => 'Coltrane Maverick', 'email' => 'coltmav@gmail.com', 'password' => Hash::make('mavcol00'), 'balance' => 103000, 'role_id' => 1, 'tier_id' => 4, 'fame' => 0, 'free_recipes_count' => 0, 'paid_recipes_count' => 0],
+           ['id' => 10, 'name' => 'William Poernomo', 'email' => 'willpoer@gmail.com', 'password' => Hash::make('butterpo12'), 'balance' => 590000, 'role_id' => 2, 'tier_id' => 5, 'fame' => 190, 'free_recipes_count' => 5, 'paid_recipes_count' =>0],
+        ]);
     }
 }
