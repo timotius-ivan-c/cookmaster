68865bbec451dad11425306d070808e05b52caa5
timotius-ivan-c <timotius.casey@binus.ac.id>
2021-01-01 21:53:45 +0700

    fixed eloquent relation between Review-User

--- cookmaster/app/Review.php
+++ cookmaster/app/Review.php
@@ -8,7 +8,7 @@ class Review extends Model
 {
     public function user()
     {
-        return $this->hasMany(User::class);
+        return $this->belongsTo(User::class);
     }
     public function recipe()
     {
--- cookmaster/app/User.php
+++ cookmaster/app/User.php
@@ -30,7 +30,7 @@ class User extends Authenticatable
 
     public function review()
     {
-        return $this->belongsTo(Review::class);
+        return $this->hasMany(Review::class);
     }
     public function favoritedRecipe()
     {
