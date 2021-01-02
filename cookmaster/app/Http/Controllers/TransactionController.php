7c57442a101993d36d5c8b25942e76d01ce80e68
jessrellia <jess.aurellia@gmail.com>
2021-01-02 14:47:51 +0700

    Added view_transaction_history function

--- cookmaster/app/Http/Controllers/TransactionController.php
+++ cookmaster/app/Http/Controllers/TransactionController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
     public function view_transaction_history(){
-        
+        $transactions = Transaction::where('sender_id', Auth::user()->id)->get();
+
+        return view('view_transaction_history', ['transactions' => $transactions]);
     }
 }
