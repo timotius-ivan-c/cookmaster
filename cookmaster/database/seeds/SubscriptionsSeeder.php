eb4fc5de8d92faed1655014e42461cc08328e4e4
jessrellia <jess.aurellia@gmail.com>
2021-01-02 19:27:53 +0700

    Updated seeders

--- cookmaster/database/seeds/SubscriptionsSeeder.php
+++ cookmaster/database/seeds/SubscriptionsSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class SubscriptionsSeeder extends Seeder
 {
@@ -11,6 +13,10 @@ class SubscriptionsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('subscriptions')->insert([
+            ['transaction_id' => 4, 'chef_id' => 5, 'member_id' => 6, 'start' => Carbon::create(2020, 12, 18,  10, 30, 14, 'Asia/Jakarta'), 'duration' => 12, 'end' => Carbon::create(2021, 12, 18,  10, 30, 14, 'Asia/Jakarta')],
+            ['transaction_id' => 8, 'chef_id' => 10, 'member_id' => 6, 'start' => Carbon::create(2021, 01, 01,  15, 32, 42, 'Asia/Jakarta'), 'duration' => 12, 'end' => Carbon::create(2022, 01, 01,  15, 32, 42, 'Asia/Jakarta')],
+            ['transaction_id' => 10, 'chef_id' => 10, 'member_id' => 7, 'start' => Carbon::create(2020, 12, 31,  23, 58, 44, 'Asia/Jakarta'), 'duration' => 12, 'end' => Carbon::create(2020, 12, 31,  23, 58, 44, 'Asia/Jakarta')],
+        ]);
     }
 }
--- cookmaster/database/seeds/TransactionsSeeder.php
+++ cookmaster/database/seeds/TransactionsSeeder.php
@@ -17,12 +17,15 @@ class TransactionsSeeder extends Seeder
         DB::table('transactions')->insert([
             ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 150000, 'date' => Carbon::create(2020, 12, 16,  9, 01, 01, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
             ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 500000, 'date' => Carbon::create(2020, 12, 17,  10, 01, 01, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
+            ['recipient_id' => 5, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 150000, 'date' => Carbon::create(2020, 12, 18,  10, 30, 14, 'Asia/Jakarta'), 'message' => 'Subscribed to Frank Conner', 'transaction_type_id' => 1],
             ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 1000000, 'date' => Carbon::create(2020, 12, 20,  20, 02, 00, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
             ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 150000, 'date' => Carbon::create(2020, 12, 25,  21, 31, 11, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
             ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 175000, 'date' => Carbon::create(2020, 12, 26,  10, 51, 14, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
-            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 300000, 'date' => Carbon::create(2020, 12, 31,  23, 58, 44, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
-
-            ['recipient_id' => NULL, 'sender_id' => 5, 'token' => Str::random(10), 'amount' => 30000, 'date' => Carbon::create(2021, 01, 01,  9, 11, 11, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1]
+            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 50000, 'date' => Carbon::create(2020, 12, 31,  23, 58, 44, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],   
+            ['recipient_id' => 10, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 100000, 'date' => Carbon::create(2021, 01, 01,  15, 32, 42, 'Asia/Jakarta'), 'message' => 'Subscribed to William Poernomo', 'transaction_type_id' => 1],
+            
+            ['recipient_id' => NULL, 'sender_id' => 7, 'token' => Str::random(10), 'amount' => 150000, 'date' => Carbon::create(2021, 01, 01,  9, 11, 11, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
+            ['recipient_id' => 10, 'sender_id' => 7, 'token' => Str::random(10), 'amount' => 100000, 'date' => Carbon::create(2020, 12, 31,  23, 58, 44, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
         ]);
     }
 }
