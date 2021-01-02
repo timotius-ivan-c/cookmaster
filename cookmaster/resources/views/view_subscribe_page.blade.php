b3ce757d13d1dbf21d5800462e634478b9cf783d
jessrellia <jess.aurellia@gmail.com>
2021-01-02 22:40:07 +0700

    Updated subscribe page

--- cookmaster/resources/views/view_subscribe_page.blade.php
+++ cookmaster/resources/views/view_subscribe_page.blade.php
@@ -1,8 +1,33 @@
-@foreach($subscriptions as $subscription)
-    {{$subscription->chef_id}}
-    {{$subscription->member_id}}
-    {{$subscription->start}}
-    {{$subscription->duration}} months
-    {{$subscription->end}}
-    <br>
-@endforeach
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    {{-- Data chef --}}
+    {{$chef->id}}
+    {{$chef->name}}
+
+    <form action='/subscribed' method='post'>
+        {{csrf_field()}}
+        <div class="form-group">
+            <input type="hidden" name="id" value="{{$chef->id}}">
+
+            <label for="duration">Duration:</label>
+                <select class="custom-select" name="duration">
+                <option selected value=0>Choose Duration</option>
+                <option value=1>1 month</option>
+                <option value=3>3 months</option>
+                <option value=6>6 months</option>
+                <option value=12>12 months</option>
+                <option value=24>24 months</option>
+            </select>
+
+            <label for="message">Message:</label>
+            <input type="text" name="message" class="form-control" id="message" placeholder="Enter message">
+            
+            @if($errors->any())
+                <div class="errors">{{ implode('', $errors->get('duration'))}}</div>
+            @endif
+        </div>
+        <button type="submit" class="btn btn-primary">Subscribe</button>
+    </form>
+</div>
+@endsection
