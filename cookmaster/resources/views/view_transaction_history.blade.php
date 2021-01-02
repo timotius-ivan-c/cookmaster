ca25645add53b94308498b7629786e46753366f9
timotius-ivan-c <timotius.casey@binus.ac.id>
2021-01-03 00:58:52 +0700

    added success message, beautify the layout a bit

--- cookmaster/resources/views/view_transaction_history.blade.php
+++ cookmaster/resources/views/view_transaction_history.blade.php
@@ -3,13 +3,13 @@
 <div>
     {{-- udah bekerja, tinggal formatting + sesuaiin apa aja yang mau ditampilkan--}}
     {{-- kalau mau dicoba pake seeding doang, login sbg soobin@gmail.com , password: csb0512--}}
+    @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
     @foreach($transactions as $transaction)
-        id:{{$transaction->id}}
-        senderid:{{$transaction->sender_id}}
-        Amount:{{$transaction->amount}}
-        Date:{{$transaction->date}}
-        Message:{{$transaction->message}}
-        <br>
+        <div class="card">
+            <span>id: {{$transaction->id}} - senderid: {{$transaction->sender_id}} - Amount: {{$transaction->amount}} - Date: {{$transaction->date}} - Message: {{$transaction->message}}</span>
+        </div>
     @endforeach
 </div>
 @endsection
