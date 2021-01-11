461cc9f7452d5d8937781affae1a42dce78dd36e
timotius-ivan-c <timotius.casey@binus.ac.id>
2021-01-11 20:50:54 +0700

    added blade for filtered recipe searches

---
+++ cookmaster/resources/views/view_recipes_test.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Recipes with ').$filter.': '.$query }}</div>
+
+                <div class="card-body">
+                    <div class="alert alert-warning">{{ __('You are viewing filtered recipes page!') }}</div>
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    @forelse($recipes as $recipe)
+                        <div>Title: {{ $recipe->name }}</div>
+                        <div>Author: {{ $recipe->user->name }}</div>
+                        <div>Ingredients:</div>
+                        <div class="card">
+                            @forelse($recipe->recipeDetailIngredient as $ingredient)
+                                <div class="card-body">{{ $ingredient->amount }} {{ $ingredient->name }} @if(!empty($ingredient->notes)) ({{ $ingredient->notes }}) @endif</div>
+                            @empty
+                                <div class="card-body">{{ __('No ingredient added yet.') }}</div>
+                            @endforelse
+                        </div>
+                        <div>Steps:</div>
+                        <div class="card">
+                            @forelse($recipe->recipeDetailStep as $step)
+                                <div class="card-body">{{ $step->step_no }}. {{ $step->text }}</div>
+                            @empty
+                                <div class="card-body">{{ __('No steps added yet.') }}</div>
+                            @endforelse
+                        </div>
+                    @empty
+                        <div class="card">{{ __('No results') }}</div>
+                    @endforelse
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
