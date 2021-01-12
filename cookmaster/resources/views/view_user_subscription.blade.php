6a7be98d56e8bf9c9501673d4f8d5cbeda7f9eac
timotius-ivan-c <timotius.casey@binus.ac.id>
2021-01-13 02:12:13 +0700

    added chef name, and latest recipe from the chef

--- cookmaster/resources/views/view_user_subscription.blade.php
+++ cookmaster/resources/views/view_user_subscription.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.app')
 @section('content')
 <div class="row">
-    @foreach ($subscription as $subscription)
-        <div class="col-lg-4 col-md-6 col-sm-8 col-xs-8 card bg-light" {{--onclick="window.location.href = ''//resep detail yang paling baru--}} ;" style="width: 20rem;">
+    @foreach ($subscriptions as $subscription)
+        <div class="card col-md-3 bg-light ml-5" {{--onclick="window.location.href = ''//resep detail yang paling baru--}} ;" style="width: 20rem;">
             {{-- <img class="card-image-top" src="{{asset('storage/'.$product->image)}}"> gambar image resep yang paling baru--}}
+            <div class="card-body">Latest Recipe: <strong>{{ $subscription->recipe->first()->name }}</strong></div>
+            <a href="{{ url('profile', $subscription->chef->id) }}" style="text-decoration: none;">
             <div class="card-body">
-            <h5 class="card-title">{{$subscription->chef_id}}</h5>{{--harusnya nama chef--}}
+            <h5 onclick="window.location='{{ url('profile', $subscription->chef->id) }}'" class="card-title">{{$subscription->chef->name }}</h5>
             <p class="card-text">
                 Duration : {{$subscription->duration}} months<br>
                 Starts : {{$subscription->start}}<br>
                 End : {{$subscription->end}}
             </p>
             </div>
+            </a>
         </div>
         
     @endforeach
