294b3d4480d312a173551734c8733a9767758d12
timotius-ivan-c <timotius.casey@binus.ac.id>
2021-01-12 17:20:25 +0700

    fixed eloquent relation with Role

--- cookmaster/app/User.php
+++ cookmaster/app/User.php
@@ -10,7 +10,7 @@ class User extends Authenticatable
 {
     public function role()
     {
-        return $this->hasOne(Role::class);
+        return $this->belongsTo(Role::class, "role_id", "id");
     }
 
     public function tier()
