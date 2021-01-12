ecfef40e5afbaf2da0fa8d06437a9e64d0bff365
timotius-ivan-c <timotius.casey@binus.ac.id>
2021-01-12 17:20:15 +0700

    fixed Eloquent relation with User

--- cookmaster/app/Role.php
+++ cookmaster/app/Role.php
@@ -8,7 +8,7 @@ class Role extends Model
 {
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, "role_id");
     }
     //
 }
