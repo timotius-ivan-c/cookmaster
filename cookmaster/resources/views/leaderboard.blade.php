2275fddb1c6fd42f41611cbe3b53e37222c3cb72
foster0400 <chanchan0400@gmail.com>
2021-01-12 21:24:28 +0700

    leaderboard view

---
+++ cookmaster/resources/views/leaderboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+<!-- ada 2 tipe, ada chef, ada contributor, type menunjukkan tipenya -->
+{{$type}}
+<!-- users menyimpan 20 data chef/contributor -->
+{{$users}}
+@endsection
