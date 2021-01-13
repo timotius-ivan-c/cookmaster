ae7fd607592925c65a9014890eb17c7e152b906e
jessrellia <jess.aurellia@gmail.com>
2021-01-13 18:50:43 +0700

    added basic form for edit profile

--- cookmaster/resources/views/edit_profile.blade.php
+++ cookmaster/resources/views/edit_profile.blade.php
@@ -2,4 +2,19 @@
 @section('content')
 id:{{$user[0]->id}}
 name:{{$user[0]->name}}
+
+<div class="container">
+    <form method="post" action="/edit-profile">
+        {{csrf_field()}}
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="pw" placeholder="{{$user[0]->name}}" name="name">
+          </div>
+        <div class="form-group">
+            <label for="mail">Email address</label>
+            <input type="email" name="email" class="form-control" id="mail" placeholder="{{$user[0]->email}}">
+        </div>
+        <button type="submit" class="btn btn-primary">Update</button>
+    </form>
+</div>
 @endsection
