ca74c3ac2c840b569e0cd9225ef7ec19693fed60
jessrellia <jess.aurellia@gmail.com>
2021-01-14 16:00:22 +0700

    Updated transaction seeder with correct message

--- cookmaster/database/seeds/TransactionsSeeder.php
+++ cookmaster/database/seeds/TransactionsSeeder.php
@@ -15,17 +15,17 @@ class TransactionsSeeder extends Seeder
     public function run()
     {
         DB::table('transactions')->insert([
-            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 150000, 'date' => Carbon::create(2020, 12, 16,  9, 01, 01, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
-            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 500000, 'date' => Carbon::create(2020, 12, 17,  10, 01, 01, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
-            ['recipient_id' => 5, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 150000, 'date' => Carbon::create(2020, 12, 18,  10, 30, 14, 'Asia/Jakarta'), 'message' => 'Subscribed to Frank Conner', 'transaction_type_id' => 1],
-            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 1000000, 'date' => Carbon::create(2020, 12, 20,  20, 02, 00, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
-            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 150000, 'date' => Carbon::create(2020, 12, 25,  21, 31, 11, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
-            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 175000, 'date' => Carbon::create(2020, 12, 26,  10, 51, 14, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
-            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 50000, 'date' => Carbon::create(2020, 12, 31,  23, 58, 44, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],   
-            ['recipient_id' => 10, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 100000, 'date' => Carbon::create(2021, 01, 01,  15, 32, 42, 'Asia/Jakarta'), 'message' => 'Subscribed to William Poernomo', 'transaction_type_id' => 1],
+            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 150000, 'date' => Carbon::create(2020, 12, 16,  9, 01, 01, 'Asia/Jakarta'), 'message' => "Added 150000 to Choi Soobin's balance", 'transaction_type_id' => 1],
+            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 500000, 'date' => Carbon::create(2020, 12, 17,  10, 01, 01, 'Asia/Jakarta'), 'message' => "Added 500000 to Choi Soobin's balance", 'transaction_type_id' => 1],
+            ['recipient_id' => 5, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 150000, 'date' => Carbon::create(2020, 12, 18,  10, 30, 14, 'Asia/Jakarta'), 'message' => 'Subscribe to Frank Conner. Duration 12 months.', 'transaction_type_id' => 1],
+            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 1000000, 'date' => Carbon::create(2020, 12, 20,  20, 02, 00, 'Asia/Jakarta'), 'message' => "Added 1000000 to Choi Soobin's balance", 'transaction_type_id' => 1],
+            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 150000, 'date' => Carbon::create(2020, 12, 25,  21, 31, 11, 'Asia/Jakarta'), 'message' => "Added 150000 to Choi Soobin's balance", 'transaction_type_id' => 1],
+            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 175000, 'date' => Carbon::create(2020, 12, 26,  10, 51, 14, 'Asia/Jakarta'), 'message' => "Added 175000 to Choi Soobin's balance", 'transaction_type_id' => 1],
+            ['recipient_id' => NULL, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 50000, 'date' => Carbon::create(2020, 12, 31,  23, 58, 44, 'Asia/Jakarta'), 'message' => "Added 50000 to Choi Soobin's balance", 'transaction_type_id' => 1],   
+            ['recipient_id' => 10, 'sender_id' => 6, 'token' => Str::random(10), 'amount' => 100000, 'date' => Carbon::create(2021, 01, 01,  15, 32, 42, 'Asia/Jakarta'), 'message' => 'Subscribe to William Poernomo. Duration 3 months.', 'transaction_type_id' => 1],
             
-            ['recipient_id' => NULL, 'sender_id' => 7, 'token' => Str::random(10), 'amount' => 150000, 'date' => Carbon::create(2021, 01, 01,  9, 11, 11, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
-            ['recipient_id' => 10, 'sender_id' => 7, 'token' => Str::random(10), 'amount' => 100000, 'date' => Carbon::create(2020, 12, 31,  23, 58, 44, 'Asia/Jakarta'), 'message' => 'Top-up', 'transaction_type_id' => 1],
+            ['recipient_id' => NULL, 'sender_id' => 7, 'token' => Str::random(10), 'amount' => 150000, 'date' => Carbon::create(2021, 01, 01,  9, 11, 11, 'Asia/Jakarta'), 'message' => "Added 150000 to Choi Soobin's balance", 'transaction_type_id' => 1],
+            ['recipient_id' => 10, 'sender_id' => 7, 'token' => Str::random(10), 'amount' => 100000, 'date' => Carbon::create(2020, 12, 31,  23, 58, 44, 'Asia/Jakarta'), 'message' => "Added 100000 to Choi Soobin's balance", 'transaction_type_id' => 1],
         ]);
     }
 }
