9bb63c4199fc633e4daae50dc285d3791dd8a3c5
jessrellia <jess.aurellia@gmail.com>
2021-01-15 21:50:07 +0700

    Updated guest chef's view

--- cookmaster/resources/views/guest_chef.blade.php
+++ cookmaster/resources/views/guest_chef.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.home_menu')
 @section('content-home')
+
 <h4 class="text-center">Guest Chef</h4>
 <div class="text-center">
     <img src="{{asset('asset/'.$recipe->image)}}" alt="{{$recipe->name}}" style="margin: 10px">
@@ -8,4 +9,5 @@
 <div class="text-center">
     <button type="button" class="btn btn-primary" onclick="window.location.href='/recipe/{{$recipe->id}}'">View Recipe</button>
 </div>
+
 @endsection
