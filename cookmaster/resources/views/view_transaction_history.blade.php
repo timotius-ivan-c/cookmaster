5bad7f1c8ef552905c41065148ae59c5132839a8
andrianpra <73392314+andrianpra@users.noreply.github.com>
2021-01-15 23:51:25 +0700

    Update view_transaction_history.blade.php

--- cookmaster/resources/views/view_transaction_history.blade.php
+++ cookmaster/resources/views/view_transaction_history.blade.php
@@ -1,15 +1,28 @@
 @extends('layouts.app')
 @section('content')
-<div>
+<div class="container">
     {{-- udah bekerja, tinggal formatting + sesuaiin apa aja yang mau ditampilkan--}}
     {{-- kalau mau dicoba pake seeding doang, login sbg soobin@gmail.com , password: csb0512--}}
     @if(session('success'))
         <div class="alert alert-success">{{ session('success') }}</div>
     @endif
-    @foreach($transactions as $transaction)
-        <div class="card">
-            <span>id: {{$transaction->id}} - senderid: {{$transaction->sender_id}} - Amount: {{$transaction->amount}} - Date: {{$transaction->date}} - Message: {{$transaction->message}}</span>
-        </div>
+    <table class="table col-lg-12">
+        <thead>
+            <tr>
+                <td>TransactionID</td>
+                <td>Amount</td>
+                <td>Date</td>
+                <td>Transaction Description</td>
+            </tr>
+        </thead>
+        @foreach($transactions as $transaction)
+        <tr>
+        <td>{{$transaction->id}}</td>
+        <td>{{$transaction->amount}}</td> 
+        <td>{{$transaction->date}}</td> 
+        <td>{{$transaction->message}}</td>
+        </tr>   
     @endforeach
+    </table>
 </div>
 @endsection
