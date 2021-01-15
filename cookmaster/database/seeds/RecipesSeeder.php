d751eebbd336148c9e02e99fe42169d01fa57043
kendricks9 <kendricksaputra14@gmail.com>
2021-01-15 20:48:32 +0700

    menambah 4 recipe di seeder

--- cookmaster/database/seeds/RecipesSeeder.php
+++ cookmaster/database/seeds/RecipesSeeder.php
@@ -15,7 +15,7 @@ class RecipesSeeder extends Seeder
     {
         DB::table('recipes')->insert([
             ['author_id' => 5, 'name' => 'Classic Fried Rice', 'image' => 'Fried_Rice.jpg', 'review_count' => 15, 'average_rating' => 5, 'publish_date' => Carbon::create(2020, 8, 16,  9, 01, 01, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 1],
-            ['author_id' => 5, 'name' => ' Honey Lime Chicken Thighs', 'image' => ' Honey_Lime_Chicken_Thighs.jpg', 'review_count' => 28, 'average_rating' => 4, 'publish_date' => Carbon::create(2019, 9, 15,  9, 03, 30, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 2],
+            ['author_id' => 5, 'name' => 'Honey Lime Chicken Thighs', 'image' => ' Honey_Lime_Chicken_Thighs.jpg', 'review_count' => 28, 'average_rating' => 4, 'publish_date' => Carbon::create(2019, 9, 15,  9, 03, 30, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 2],
             ['author_id' => 5, 'name' => 'Tofu Avocado Salad', 'image' => 'Tofu_Avocado.jpg', 'review_count' => 23, 'average_rating' => 3, 'publish_date' => Carbon::create(2020, 8, 17,  23, 45, 01, 'Asia/Jakarta'), 'recipe_type' => 2, 'recipe_category_id' => 3],
             ['author_id' => 5, 'name' => 'Spaghetti Carbonara', 'image' => 'spaghetti_carbonara.jpg', 'review_count' => 67, 'average_rating' => 4, 'publish_date' => Carbon::create(2020, 9, 24,  14, 15, 15, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 3],
             ['author_id' => 5, 'name' => 'Peanut Noodles', 'image' => 'peanut-sauce-noodles.jpg', 'review_count' => 49, 'average_rating' => 4, 'publish_date' => Carbon::create(2020, 9, 14,  9, 30, 01, 'Asia/Jakarta'), 'recipe_type' => 2, 'recipe_category_id' => 1],
@@ -25,10 +25,10 @@ class RecipesSeeder extends Seeder
             ['author_id' => 10, 'name' => 'Parmesan Crusted Chicken', 'image' => 'Parmesan_Chicken.jpg', 'review_count' => 18, 'average_rating' => 5.00, 'publish_date' => Carbon::create(2020, 10, 16,  8, 11, 12, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 3],
             ['author_id' => 10, 'name' => 'Oreo Truffles', 'image' => 'Oreo_Truffles.jpg', 'review_count' => 15, 'average_rating' => 4.88, 'publish_date' => Carbon::create(2019, 8, 15,  19, 32, 11, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 3],
             ['author_id' => 14, 'name' => 'Red Braised Pork Belly', 'image' => 'Braised_Pork_Belly.jpg', 'review_count' => 10, 'average_rating' => 4.90, 'publish_date' => Carbon::create(2018, 1, 1,  23, 30, 03, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 2],
-            ['author_id' => 14, 'name' => '', 'image' => '', 'review_count' => 15, 'average_rating' => 4.88, 'publish_date' => Carbon::create(2019, 8, 15,  19, 32, 11, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 2],
-            ['author_id' => 15, 'name' => '', 'image' => '', 'review_count' => 15, 'average_rating' => 4.88, 'publish_date' => Carbon::create(2019, 8, 15,  19, 32, 11, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 2],
-            ['author_id' => 15, 'name' => '', 'image' => '', 'review_count' => 15, 'average_rating' => 4.88, 'publish_date' => Carbon::create(2019, 8, 15,  19, 32, 11, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 2],
-            ['author_id' => 15, 'name' => '', 'image' => '', 'review_count' => 15, 'average_rating' => 4.88, 'publish_date' => Carbon::create(2019, 8, 15,  19, 32, 11, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 2],
+            ['author_id' => 14, 'name' => 'Kung Pao Chicken', 'image' => 'Kung_Pao.jpg', 'review_count' => 15, 'average_rating' => 4.88, 'publish_date' => Carbon::create(2019, 8, 15,  19, 32, 11, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 2],
+            ['author_id' => 15, 'name' => 'Beef and Broccoli', 'image' => 'Beef_and_Brocoli.jpg', 'review_count' => 15, 'average_rating' => 4.88, 'publish_date' => Carbon::create(2019, 8, 15,  19, 32, 11, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 1],
+            ['author_id' => 15, 'name' => 'Chicken Satay', 'image' => 'Chicken_Satay.jpg', 'review_count' => 15, 'average_rating' => 4.88, 'publish_date' => Carbon::create(2019, 8, 15,  19, 32, 11, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 1],
+            ['author_id' => 15, 'name' => 'Glazed Roast Duck', 'image' => 'Glazed_Roast_Duck.jpg', 'review_count' => 15, 'average_rating' => 4.88, 'publish_date' => Carbon::create(2019, 8, 15,  19, 32, 11, 'Asia/Jakarta'), 'recipe_type' => 1, 'recipe_category_id' => 1],
         ]);
     }
 }
