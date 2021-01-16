b5680910dc3b3ddcb3d1ee840b9d57fa20005f70
jessrellia <jess.aurellia@gmail.com>
2021-01-16 20:30:25 +0700

    Handler for 404 page

--- cookmaster/app/Exceptions/Handler.php
+++ cookmaster/app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +51,13 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if($this->isHttpException($exception)){
+            if($exception instanceof HttpExceptionInterface){
+                if($exception->getStatusCode()=='404'){
+                    return response()->view('error_404');
+                }
+            }
+        }
         return parent::render($request, $exception);
     }
 }
