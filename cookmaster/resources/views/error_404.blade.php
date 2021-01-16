a93e3a134f8112280cc469d9ce193b58c61c37b0
jessrellia <jess.aurellia@gmail.com>
2021-01-16 20:33:35 +0700

    Blade for 404 error

---
+++ cookmaster/resources/views/error_404.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+<body>
+    <div class="page-not-found"></div>
+    <button type="button" class="btn btn-dark err-404" onclick="window.location.href='/home'">RETURN HOME</button>
+</body>
+@endsection
