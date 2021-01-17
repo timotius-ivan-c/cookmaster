114cc714cf725d943bda208178cf67c5209ae8e9
timotius-ivan-c <timotius.casey@binus.ac.id>
2021-01-17 13:04:27 +0700

    removed Ingredient and Steps display

--- cookmaster/resources/views/view_filtered_recipes.blade.php
+++ cookmaster/resources/views/view_filtered_recipes.blade.php
@@ -9,8 +9,8 @@
             <div class="card">
                 <div class="card-header">{{ __('Recipes with ').$filter.': '.$query }}</div>
 
+                <div class="alert alert-info">{{ __('Click on any card to view the full recipe') }}</div>
                 <div class="card-body">
-                    <div class="alert alert-warning">{{ __('You are viewing filtered recipes page!') }}</div>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -21,30 +21,16 @@
                     <div class="card mb-5">
                         <div class="card-header bg-warning">Paid Recipes from Your Subscriptions</div>
                         @forelse($recipes_paid as $recipe)
-                        <div class="card m-2">
-                            <div class="card-header">
-                                <div>Title: {{ $recipe->name }}</div>
-                                <div>Author: {{ $recipe->user->name }}</div>
-                            </div>
-                            <div class="card-body">Ingredients:</div>
-                            <div class="card">
-                                @forelse($recipe->recipeDetailIngredient as $ingredient)
-                                    <div class="card-body">{{ $ingredient->amount }} {{ $ingredient->name }} @if(!empty($ingredient->notes)) ({{ $ingredient->notes }}) @endif</div>
-                                @empty
-                                    <div class="card-body">{{ __('No ingredient added yet.') }}</div>
-                                @endforelse
-                            </div>
-                            <div class="card-body">Steps:</div>
-                            <div class="card">
-                                @forelse($recipe->recipeDetailStep as $step)
-                                    <div class="card-body">{{ $step->step_no }}. {{ $step->text }}</div>
-                                @empty
-                                    <div class="card-body">{{ __('No steps added yet.') }}</div>
-                                @endforelse
+                        <div class="card m-2" onclick="window.location='/recipe/view-recipe/{{$recipe->id}}'">
+                            <div class="card-header"><a href="/recipe/view-recipe/{{$recipe->id}}">{{ $recipe->name }}</a></div>
+                            <div class="card-body">
+                                <div>Author: <a href="/profile/{{$recipe->user->id}}">{{ $recipe->user->name }}</a></div>
+                                <div>Rating: {{ $recipe->average_rating }} / 5</div>
+                                <div>Reviews: {{ $recipe->review_count }}</div>
                             </div>
                         </div>
                         @empty
-                            <div class="card">{{ __('No results') }}</div>
+                            <div class="card-body">{{ __('No results') }}</div>
                         @endforelse
                     </div>
                     @endif
@@ -52,30 +38,16 @@
                     <div class="card">
                         <div class="card-header bg-danger">Free Recipes</div>
                         @forelse($recipes as $recipe)
-                        <div class="card m-2">
-                            <div class="card-header">
-                                <div>Title: {{ $recipe->name }}</div>
-                                <div>Author: {{ $recipe->user->name }}</div>
-                            </div>
-                            <div class="card-body">Ingredients:</div>
-                            <div class="card">
-                                @forelse($recipe->recipeDetailIngredient as $ingredient)
-                                    <div class="card-body">{{ $ingredient->amount }} {{ $ingredient->name }} @if(!empty($ingredient->notes)) ({{ $ingredient->notes }}) @endif</div>
-                                @empty
-                                    <div class="card-body">{{ __('No ingredient added yet.') }}</div>
-                                @endforelse
-                            </div>
-                            <div class="card-body">Steps:</div>
-                            <div class="card">
-                                @forelse($recipe->recipeDetailStep as $step)
-                                    <div class="card-body">{{ $step->step_no }}. {{ $step->text }}</div>
-                                @empty
-                                    <div class="card-body">{{ __('No steps added yet.') }}</div>
-                                @endforelse
+                        <div class="card m-2" onclick="window.location='/recipe/view-recipe/{{$recipe->id}}'">
+                            <div class="card-header"><a href="/recipe/view-recipe/{{$recipe->id}}">{{ $recipe->name }}</a></div>
+                            <div class="card-body">
+                                <div>Author: <a href="/profile/{{$recipe->user->id}}">{{ $recipe->user->name }}</a></div>
+                                <div>Rating: {{ $recipe->average_rating }} / 5</div>
+                                <div>Reviews: {{ $recipe->review_count }}</div>
                             </div>
                         </div>
                         @empty
-                            <div class="card">{{ __('No results') }}</div>
+                            <div class="card-body">{{ __('No results') }}</div>
                         @endforelse
                     </div>
                 </div>
