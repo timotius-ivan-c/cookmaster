8bc6d7bda2ef42eeccdf935880c1adf01528aec0
timotius-ivan-c <timotius.casey@binus.ac.id>
2021-01-17 15:38:57 +0700

    added img and link of latest recipe

--- cookmaster/resources/views/view_user_subscription.blade.php
+++ cookmaster/resources/views/view_user_subscription.blade.php
@@ -6,9 +6,11 @@
 <div class="container">
 <div class="row">
     @foreach ($subscriptions as $subscription)
-        <div class="card col-md-3 bg-light ml-5" {{--onclick="window.location.href = ''//resep detail yang paling baru--}} ;" style="width: 20rem;">
-            {{-- <img class="card-image-top" src="{{asset('storage/'.$product->image)}}"> gambar image resep yang paling baru--}}
-            <div class="card-body">Latest Recipe: <strong>{{ $subscription->recipe->first()->name }}</strong></div>
+        <div class="card col-md-3 bg-light ml-5" style="width: 24rem;">
+            <div onclick="window.location='/recipe/view-recipe/{{$subscription->recipe->first()->id}}'">
+                <img class="img-thumbnail" src="{{asset('storage/'.$subscription->recipe->first()->image)}}">
+                <div class="card-body">Latest Recipe: <br><strong><a href="/recipe/view-recipe/{{$subscription->recipe->first()->id}}">{{ $subscription->recipe->first()->name }}</a></strong></div>
+            </div>
             <a href="{{ url('profile', $subscription->chef->id) }}" style="text-decoration: none;">
             <div class="card-body">
             <h5 onclick="window.location='{{ url('profile', $subscription->chef->id) }}'" class="card-title">{{$subscription->chef->name }}</h5>
