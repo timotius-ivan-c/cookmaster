7ff02ce0ac7aa32e8072aca878d6534cdb4fea1f
timotius-ivan-c <timotius.casey@binus.ac.id>
2021-01-17 15:40:24 +0700

    add hover effect, rcp from fllwings and subscription

--- cookmaster/resources/views/welcome.blade.php
+++ cookmaster/resources/views/welcome.blade.php
@@ -62,6 +62,11 @@
                 margin-top: 5px;
             }
         }
+
+        .card>.card:hover{
+            transform: scale(1.002);
+            box-shadow: 0 10px 20px rgba(0,0,0,.12), 0 4px 8px rgba(0,0,0,.06);
+        }
     </style>
     <script>
         $(document).ready(function(){
@@ -77,7 +82,10 @@
                         window.location = "/recipe/ingredient/"+str;
                     }
                 }
-            })
+            });
+            $("#edit_recipe").click(function(event) {
+                event.stopPropagation();
+            });
         });
     </script>
 </head>
@@ -89,7 +97,7 @@
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-1">
+                <ul class="navbar-nav mr-auto">
                     <input class="form-control mr-sm-2 col-lg-6" type="text" placeholder="Search" id="search" name="search" aria-label="Search">
                     <select name="choose" id="choose">
                         <option value="recipe">Recipe</option>
@@ -98,9 +106,9 @@
                     <button class="btn btn-primary my-2 my-sm-0" type="button" id="myBtn">Search</button>
                 </ul>
                 <ul class="navbar-nav mr-auto">
-                <a class="navbar-brand  text-center" href="{{ url('/') }}">
-                    <font size ="6px">cook master</font><br><font size="3px"><i>anyone can cook</i></font>
-                </a>
+                    <a class="navbar-brand  text-center" href="{{ url('/') }}">
+                        <font size ="6px">cook master</font><br><font size="3px"><i>anyone can cook</i></font>
+                    </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -123,13 +131,12 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="">Profile</a>
+                                <a class="dropdown-item" href="/profile/{{Auth::user()->id}}">Profile</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
-                                
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
@@ -140,17 +147,21 @@
                         @if(\Auth::user()->role_id == 1)
                             <button type="button" class="btn btn-primary mr-sm-2" onclick="window.location.href='/subscriptions'">Subscription</button>
                         @elseif(\Auth::user()->role_id==2||\Auth::user()->role_id==3)
-                            <button type="button" class="btn btn-primary mr-sm-2" onclick="window.location.href='/'">Earnings</button>
+                            <button type="button" class="btn btn-primary mr-sm-2" onclick="window.location.href='/earning'">Earnings</button>
                         @endif
                     @endguest
                 </ul>
             </div>
         </div>
     </nav>
+    
     <div class="container">
         <ul class="nav justify-content-center" style="background-color: #e3f2fd;">
+            <li>
+                <a class="nav-link" href="/home">HOME</a>
+            </li>
             <li class="nav-item">
-                <a class="nav-link" href="">FEATURED</a>
+                <a class="nav-link" href="/recipe/best-recipe">BEST RECIPE</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/guest-chef">GUEST CHEF</a>
@@ -164,12 +175,12 @@
             @if(\Auth::user()->role_id==2||\Auth::user()->role_id==3)
             <br>
             <div class="container adm-btns">
-                <button type="button" class="btn btn-primary" onclick="window.location.href=">Add New Recipe</button>
+                <button type="button" class="btn btn-primary" onclick="window.location='/new-recipe/'">Add New Recipe</button>
             </div>
             <br>
             <div class="container">
             <div class="row">
-                <div class="card col-md-3 bg-light ml-5" {{--onclick="window.location.href = ''//Earnings page--}} ;" style="width: 40rem;">
+                <div class="card col-md-3 bg-light ml-5" onclick="window.location='/earning'" style="width: 40rem;">
                     <div class="card-body"><h5><strong>Report:</strong></h5><br>
                         <p class="card-text">
                             Balance : <br>Rp {{$user->balance}}<br>
@@ -181,7 +192,7 @@
                         </p>
                     </div>
                 </div>
-                <div class="card col-md-3 bg-light ml-5" {{--onclick="window.location.href = ''//Latest recipe page--}} style="width: 40rem;">
+                <div class="card col-md-3 bg-light ml-5" onclick="window.location.href='/recipe/view-recipe/{{$my_recipes->first()->id}}'" style="width: 40rem;">
                     <div class="card-body"><h5><strong>Latest Recipe:</strong></h5></div>
                     <img class="card-image-top" src="{{asset('storage/'.$my_recipes->first()->image)}}"> 
                     <div class="card-body">   
@@ -190,50 +201,84 @@
                         Average rating  : <br>
                             {{$my_recipes->first()->average_rating}} / 5.00 <br> from {{$my_recipes->first()->review_count}} people <br><br>
                         <div class="container adm-btns">
-                            <button type="button" class="btn btn-primary" onclick="window.location.href='/edit-recipe/{{$my_recipes->first()->id}}">Edit Recipe</button>
+                            <button type="button" class="btn btn-primary" id="edit_recipe" onclick="window.location='/edit-recipe/{{$my_recipes->first()->id}}'">Edit Recipe</button>
                         </div>
                     </div>
                 </div>
             </div>
             </div>
             @elseif(\Auth::user()->role_id==1)
-            <div class="row">
-                <div class="card col-md-3 bg-light ml-5" {{--onclick="window.location.href = ''//Earnings page--}} ;" style="width: 40rem;">
-                    <div class="card-body"><h5><strong>Report:</strong></h5><br>
-                        <p class="card-text">
-                            Balance : <br>Rp. {{$user->balance}}<br>
-                            <br>
-                            Lifetime topup :<br>Rp. {{$user->lifetime_topup}}<br>
-                        </p>
-                    </div>
-                </div>
-                @forelse($recipes as $recipe)
-                <div class="card col-md-3 bg-light ml-5" {{--onclick="window.location.href = ''//Latest recipe page--}} style="width: 40rem;">
-                    <div class="card-body"><h5><strong>Latest Recipe: </strong></h5></div>
-                    <img class="card-image-top" src="{{asset('storage/'.$recipes->first()->image)}}"> 
-                    <div class="card-body">   
-                    <div class="card-title"><strong>{{$recipes->name}}</strong></div>
-                    <div class="card-text">        
-                        Average rating  : <br>
-                            {{$recipes->average_rating}} / 5.00 <br> from {{$recipes->review_count}} people <br><br>
-                        <div class="container adm-btns">
-                            <button type="button" class="btn btn-primary" onclick="window.location.href='/edit-recipe/{{$recipes->id}}'">Edit Recipe</button>
+            <br>
+            <div class="container">
+                <div class="row">
+                    <!-- Recipe from followings -->
+                    <div class="card ml-3">
+                        <div class="card-header h5">Latest recipes from people you follow</div>
+                        @forelse($recipes as $recipe)
+                        <div class="card bg-light" style="width: 25rem;">
+                            <div class="card-body"><h5><strong>{{ $recipe->name }} </strong></h5></div>
+                            <img class="card-image-top" src="{{asset('storage/'.$recipe->image)}}"> 
+                            <div class="card-body">
+                                <div class="card-title"><strong>From <a href="/profile/{{$recipe->user->id}}">{{ $recipe->user->name }}</a></strong></div>
+                                <div class="card-text">        
+                                    Average rating  : <br>
+                                        {{$recipe->average_rating}} / 5.00 <br> from {{$recipe->review_count}} people <br><br>
+                                    <div class="container adm-btns">
+                                        <button type="button" class="btn btn-primary" onclick="window.location.href='/recipe/view-recipe/{{$recipe->id}}'">View Recipe</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        @empty
+                        <div class="card-body" style="width: 25rem;">You are not following anyone.</div>
+                        @endforelse
+                    </div>
+
+                    <!-- Recipes from subscriptions -->
+                    <div class="card ml-2">
+                        <div class="card-header h5">Latest recipes from your subscriptions</div>
+                        @forelse($subscriptions as $recipe)
+                        <div class="card bg-light" style="width: 25rem;">
+                            <div class="card-body"><h5><strong>{{$recipe->name}}</strong></h5></div>
+                            <img class="card-image-top" src="{{asset('storage/'.$recipe->image)}}"> 
+                            <div class="card-body">
+                                <div class="card-title"><strong>From <a href="/profile/{{$recipe->user->id}}">{{ $recipe->user->name }}</a></strong></div>
+                                <div class="card-text">        
+                                    Average rating  : <br>
+                                        {{$recipe->average_rating}} / 5.00 <br> from {{$recipe->review_count}} people <br><br>
+                                    <div class="container adm-btns">
+                                        <button type="button" class="btn btn-primary" onclick="window.location.href='/recipe/view-recipe/{{$recipe->id}}'">View Recipe</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @empty
+                        <div class="card-body" style="width: 25rem;">You don't have any active subscription.</div>
+                        @endforelse
+                    </div>
+                    
+                    <div class="card col-md-3 bg-light ml-2 pb-4" style="width: 40rem; height: fit-content;">
+                        <div class="card-body"><h5><strong>Your Account:</strong></h5><br>
+                            <p class="card-text">
+                                Balance : <br>Rp. {{$user->balance}}<br>
+                                <br>
+                                Lifetime topup :<br>Rp. {{$user->lifetime_topup}}<br>
+                            </p>
                     </div>
                 </div>
-                @empty
-                @endforelse
             </div>
+
             </div>
             @endif
         @endauth
         <br>
         <br>
         <div class="container">
-            <h5>Best Recipes: </h5>
-            <br>
+            <div class="card">
+            <div class="card-header"><h5>Best Recipes: </h5></div>
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
+                <div class="carousel-inner bg-light">
+                <br>
                   <div class="carousel-item active" onclick="window.location='/recipe/view-recipe/{{$best_recipes[0]->id}}'">
                     <img class="d-block w-200" src="{{asset('storage/'.$best_recipes[0]->image)}}" alt="{{$best_recipes[0]->name}}">
                     <div class="d-block w-200 h4" style="text-align: center;">{{$best_recipes[0]->name}}</div>
@@ -247,15 +292,17 @@
                     <div class="d-block w-200 h4" style="text-align: center;">{{$best_recipes[2]->name}}</div>
                   </div>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <a class="carousel-control-prev bg-secondary" href="#carouselExampleControls" role="button" data-slide="prev">
                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
+                  <span class="sr-only bg-dark">Previous</span>
                 </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <a class="carousel-control-next bg-dark" href="#carouselExampleControls" role="button" data-slide="next">
                   <span class="carousel-control-next-icon" aria-hidden="true"></span>
                   <span class="sr-only">Next</span>
                 </a>
-              </div>        
+            </div>
+        </div>
+        <br><br>
     </div>
 </body>
 </html>
