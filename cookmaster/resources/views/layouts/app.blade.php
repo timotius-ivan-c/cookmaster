bc80202da467d9a9eaa205879d77c9098851ccee
kendricks9 <kendricksaputra14@gmail.com>
2021-01-17 13:56:54 +0700

    menambahkan href earning dan profile di layouts.app

--- cookmaster/resources/views/layouts/app.blade.php
+++ cookmaster/resources/views/layouts/app.blade.php
@@ -219,7 +219,7 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="">Profile</a>
+                                <a class="dropdown-item" href="/profile/{{Auth::user()->id}}">Profile</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
@@ -235,7 +235,7 @@
                         @if(\Auth::user()->role_id == 1)
                             <button type="button" class="btn btn-primary mr-sm-2" onclick="window.location.href='/subscribe/{user:id}'">Subscription</button>
                         @elseif(\Auth::user()->role_id==2||\Auth::user()->role_id==3)
-                            <button type="button" class="btn btn-primary mr-sm-2" onclick="window.location.href='/'">Earnings</button>
+                            <button type="button" class="btn btn-primary mr-sm-2" onclick="window.location.href='/earning/{user:id}'">Earnings</button>
                         @endif
                     @endguest
                 </ul>
