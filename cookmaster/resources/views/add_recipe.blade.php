865ac97c0793641ed7ed8ecd5086e13f09589a3f
timotius-ivan-c <timotius.casey@binus.ac.id>
2021-01-17 14:50:59 +0700

    added cancel button

--- cookmaster/resources/views/add_recipe.blade.php
+++ cookmaster/resources/views/add_recipe.blade.php
@@ -15,7 +15,7 @@
                 @endforeach
             </div>
         @endif
-        {{csrf_field()}}
+        @csrf
         <div class="form-group">
             <label for="name">Recipe Name</label>
             <input type="text" class="form-control" id="pw" placeholder="Input recipe title . . ." name="name">
@@ -51,82 +51,9 @@
         <input type="hidden" name="step" value="0">
 
         <button type="submit" class="btn btn-primary">Next >></button>
+        <button type="reset" class="btn btn-danger" onclick="window.location='/home/'">Cancel</button>
     </form>
     @endif
-    {{-- @else
-        <div class="card-body">
-            Name: {{ $recipe->name}}
-        </div>
-        <div class="card-body">
-            Category" {{ $recipe->recipeCategory->name }}
-        </div>
-        <div class="card-body">
-            Type: @if($recipe->recipe_type == 1) Free Recipe @else Paid Recipe @endif
-        </div>
-        <div class="card">
-            <div class="card-header">
-                Ingredients:
-            </div>
-            @forelse($recipe->recipeDetailIngredient as $ingredient)
-            <div class="card-body">{{ $ingredient->amount }} {{ $ingredient->name }} @if(!empty($ingredient->notes)) ({{ $ingredient->notes }}) @endif</div>
-            @empty
-            @endforelse
-
-            @if($step == 1)
-            <form action="{{route('recipe.add')}}" method="post">
-                {{csrf_field()}}
-                <input type="hidden" name="step" id="step" value="1">
-                <input type="hidden" name="recipe_id" id="recipe_id" value="{{$recipe->id}}">
-                <input type="text" name="name" id="name" placeholder="ingredient name">
-                <input type="text" name="amount" id="amount" placeholder="e.g. 3 tbsp, 2 stalks">
-                <input type="text" name="notes" id="notes" placeholder="e.g. minced, chopped, diced">
-                <button type="submit" class="btn btn-primary">+ Add</button>
-            </form>
-            <div class="card-footer">
-                <form action="{{route('recipe.add')}}" method="post">
-                    {{csrf_field()}}
-                    <input type="hidden" name="recipe_id" value="{{$recipe->id}}">
-                    <input type="hidden" name="step" value="1">
-                    <input type="hidden" name="is_next" value="true">
-                    <button class="btn btn-primary" type="submit">Next >></button>
-                </form>
-            </div>
-            @endif
-        </div>
-        @if($step == 2)
-        <div class="card">
-            <div class="card-header">
-                Steps:
-            </div>
-            @forelse($recipe->recipeDetailStep as $step)
-                <img src="{{asset('storage/'.$step->image)}}"/>
-                <div class="card-body">{{ $step->step_no }}. {{ $step->text }}</div>
-            @empty
-            @endforelse
-            
-            <form action="{{route('recipe.add')}}" method="post" enctype="multipart/form-data">
-                {{csrf_field()}}
-                <input type="hidden" name="step" id="step" value="2">
-                <input type="hidden" name="recipe_id" id="recipe_id" value="{{$recipe->id}}">
-                <br><img id="image_show" src="">
-                <br><label for="upload-file" class="browse-label">Step Image</label>
-                <input type="file" class="form-control-file" id="upload-file" name="image" onchange="readURL(this)">
-                <label for="text">{{ count($recipe->recipeDetailStep)+1 }}. </label>
-                <input type="text" name="text" id="text">
-                
-                <button type="submit">+ Add</button>
-            </form>
-
-            <form action="{{route('recipe.add')}}" method="post">
-                {{csrf_field()}}
-                <input type="hidden" name="step" id="step" value="2">
-                <input type="hidden" name="recipe_id" id="recipe_id" value="{{$recipe->id}}">
-                <input type="hidden" name="is_next" value="true">
-                <button class="btn btn-primary" type="submit">Finish >></button>
-            </form>
-        </div>
-        @endif
-    @endif --}}
 </div>
 </div>
 @endsection
