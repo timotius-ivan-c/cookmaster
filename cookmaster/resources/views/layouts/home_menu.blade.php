869e3d56e66497bc3da49d0634f9a74bb8e59cc4
timotius-ivan-c <timotius.casey@binus.ac.id>
2021-01-17 15:40:32 +0700

    added HOME link

--- cookmaster/resources/views/layouts/home_menu.blade.php
+++ cookmaster/resources/views/layouts/home_menu.blade.php
@@ -4,6 +4,9 @@
 <div class="container" style="margin-bottom: 20px">
     <ul class="nav justify-content-center" style="background-color: #e3f2fd;">
         <li class="nav-item">
+            <a class="nav-link" href="/home">HOME</a>
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="/recipe/best-recipe">BEST RECIPE</a>
         </li>
         <li class="nav-item">
